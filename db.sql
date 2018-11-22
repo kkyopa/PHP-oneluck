dbbbfc8b7d3e0369e85c214c7c4bfcb6912fada1
vagrant <vagrant@vagrant.vm>
2018-11-22 06:22:46 +0000

    コメントアウト実行

--- db.sql
+++ db.sql
@@ -1,5 +1,6 @@
-デーブル作成時のコマンド
-CREATE TABLE lucks(user_id int(11),content varchar(255),image varchar(255) );
-
-データベース作成のコマンド
+#データベース作成のコマンド
 CREATE DATABASE SAMPLE
+
+
+#デーブル作成時のコマンド
+CREATE TABLE lucks(user_id int(11),content varchar(255),image varchar(255) );
