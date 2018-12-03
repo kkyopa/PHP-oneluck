e534284910cea2fa36ab696d349dc8cb510fbf6d
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2018-12-03 17:56:43 +0900

    画像機能途中段階

--- confime.php
+++ confime.php
@@ -7,6 +7,20 @@
   echo $name;
 ?>
 
+
+<?php
+  //ファイルの保存先
+  $upload = './images/'.$_FILES['file_upload']['name'];
+  //アップロードが正しく完了したかチェック
+  if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $upload)){
+    echo 'アップロード完了';
+  }else{
+    echo 'アップロード失敗';
+  }
+?>
+
+
+
 <!DOCTYPE html>
 <html>
   <head>
--- new.php
+++ new.php
@@ -10,8 +10,9 @@
   <input type="button" onClick="location.href='http://192.168.33.10:3000/new.php'" value="一日一善の投稿">
 <div>
   <h1>投稿画面</h1>
-  <form action="confime.php" method="post">
-    投稿内容：<input type="text" name="note">
+  <form action="confime.php" method="post" enctype="multipart/form-data">
+    投稿内容：<input type="text" name="note"><br>
+    画像投稿:<input name="file_upload" type="file" /><br>
     <input type="submit" value="確認する">
   </form>
 </div>
