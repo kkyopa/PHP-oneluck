1232e3f2f3f1fb0f011be70cf9e23d38f032ff83
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2018-12-13 16:30:42 +0900

    sign_up登録完了画面作成途中

---
+++ login/sign_register.php
@@ -0,0 +1,42 @@
+<?php
+$name = $_POST['note'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+require_once('config.php');
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=one_luck;charset=utf8', 'root', DB_PASSWORD);
+$sql = "INSERT INTO mypage (name) VALUES ('" . $name . "')";
+$sql = "INSERT INTO mypage (email) VALUES ('" . $email . "')";
+$sql = "INSERT INTO mypage (password) VALUES ('" . $pass . "')";
+error_log("sql=" . $sql);
+$stm = $pdo->prepare($sql);
+$stm->execute();
+$id = $pdo->lastInsertId();
+
+
+if (!empty($_POST['tmp_file_path'])) {
+    $tmp_file_path = $_POST['tmp_file_path'];
+    $arr = explode('.', $tmp_file_path);
+    $ext = end($arr);
+    $fname = $id . '.' . $ext;
+    rename($tmp_file_path, './mypage_images/' . $fname);
+    $sql = "UPDATE lucks set attach_filename = '" . $fname . "' WHERE id = ".$id;
+    error_log("sql=" . $sql);
+    $stm = $pdo->prepare($sql);
+    $stm->execute();
+}
+
+// 好みで location で リダイレクト
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>みんなの投稿</title>
+    <meta charset="utf-8">
+</head>
+<body>
+  <h1>登録が完了しました。</h1>
+<a href="/">もどる</a>
+</body>
+
+</html>
