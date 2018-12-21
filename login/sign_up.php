8ea9b4420681ed7e95319f505d80f390b0f97d4c
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2018-12-21 12:41:13 +0900

    sign_upスマーティ導入途中

--- login/sign_up.php
+++ login/sign_up.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>one_luck</title>
-    <link href="./style.css" rel="stylesheet" type="text/css">
-  </head>
-<body>
-<div>
-  <h1>サインアップ</h1>
-  <form action="sign_confime.php" method="post" enctype="multipart/form-data">
-    name  <input type="text" name="note"><br>
-    email  <input type="text" name="email"><br>
-    pass <input type="text" name="pass"><br><br>
-    image<br> <input name="file_upload" type="file" /><br><br>
-    <input type="submit" value="確認する"><br>
-  </form>
-</div>
-</body>
-</html>
+<?php
+require_once('../config.php');
+require_once('lib/smarty/Smarty.class.php');
+session_start();
+if (isset($_SESSION['id'])) {
+header('Location: http://192.168.33.10:3000/luckfile/index.php');
+  exit();
+}
+$smarty->display('sign_up.tpl');
+?>
