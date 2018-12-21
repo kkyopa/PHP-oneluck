e1bde4df58234f4db3d86b8ff29a8310e01164db
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2018-12-21 12:54:04 +0900

    Smarty クラスの呼び出しを追加

--- login/sign_up.php
+++ login/sign_up.php
@@ -6,5 +6,11 @@ if (isset($_SESSION['id'])) {
 header('Location: http://192.168.33.10:3000/luckfile/index.php');
   exit();
 }
+
+$smarty = new Smarty();
+$smarty->template_dir = '../templates/';
+$smarty->compile_dir  = '../templates_c/';
+$smarty->config_dir   = '../configs/';
+$smarty->cache_dir    = '../cache/';
 $smarty->display('sign_up.tpl');
 ?>
