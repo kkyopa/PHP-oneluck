8b26386eb018d5346c0cfcae18d09ab0d07a41e6
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2018-12-21 14:15:51 +0900

    index.phpスマーティー導入前に戻す

--- luckfile/index.php
+++ luckfile/index.php
@@ -9,7 +9,6 @@
     session_start();
     if ($_SESSION['id']) {
         require_once('../config.php');
-
         $dbh=new PDO('mysql:host=127.0.0.1;dbname=one_luck;charset=utf8', 'root', DB_PASSWORD);
         $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $sql='SELECT name FROM mypage WHERE id=?';
@@ -18,7 +17,6 @@
         $stmt->execute($params);
         $record = $stmt->fetch(PDO::FETCH_ASSOC);
         echo $record["name"] . "さん、ログイン中";
-
         echo '<a href="/login/logout.php">ログアウト</a>';
      } else { ?>
     <a href="/login/">ログイン</a>
@@ -29,38 +27,34 @@
     <h1>みんなの一日一善</h1>
 
     <?php
-
     require_once('../config.php');
-    require_once('lib/smarty/Smarty.class.php');
     $pdo=new PDO('mysql:host=127.0.0.1;dbname=one_luck;charset=utf8', 'root', DB_PASSWORD);
     $sql = "SELECT * FROM lucks";
     error_log("sql=" . $sql);
     $stm = $pdo->prepare($sql);
     $stm->execute();
     $record_list = $stm->fetchAll();
-
-
-    $record_list = $results[0];
-
-
-    $smarty = new Smarty();
-    $smarty->template_dir = '../templates/';
-    $smarty->compile_dir  = '../templates_c/';
-    $smarty->config_dir   = '../configs/';
-    $smarty->cache_dir    = '../cache/';
-
-
-
-
-    $smarty->assign("id", $record_list['id']);
-    $smarty->assign("content", $record_list['content']);
-    if ($record_list['attach_filename']) {
-        echo "<td><img src='./images/" . $record_list['attach_filename'] . "'/></td>";
-    } else {
-        echo "<td>no image</td>";
+    echo "<table border='10'>";
+    foreach($record_list as $record) {
+        echo "<tr>";
+        echo "<td>" . $record['id'] . "</td>";
+        echo "<td>" . $record['content'] . "</td>";
+        if ($record['attach_filename']) {
+            echo "<td><img src='./images/" . $record['attach_filename'] . "'/></td>";
+        } else {
+            echo "<td>no image</td>";
+        }
+       print('<td><form method="POST" action="delete.php">');
+       print('<input type="hidden" name="id" value="'.htmlspecialchars($record['id'], ENT_QUOTES | ENT_HTML5, 'UTF-8').'">');
+       print('<input type="submit" value="削除"></form></td>');
+       print('<td>');
+       echo '<a href="/luckfile/edit_menu.php?id='.$record['id'].'">編集</a>';
+       print('</td>');
+       print('<td>');
+       echo '<a href="/luckfile/show.php?id='.$record['id'].'">詳細</a>';
+       echo "</td></tr>";
     }
-    $smarty->display('index.tpl');
-
-
+    echo "</table>";
+    ?>
   </body>
 </html>
