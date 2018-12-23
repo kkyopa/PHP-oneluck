c1b4b1270a58cd4f53a40ae1d25fc65066c97373
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2018-12-23 16:41:25 +0900

    パスの変更

--- luckfile/register.php
+++ luckfile/register.php
@@ -14,7 +14,7 @@ if (!empty($_POST['tmp_file_path'])) {
     $arr = explode('.', $tmp_file_path);
     $ext = end($arr);
     $fname = $id . '.' . $ext;
-    rename($tmp_file_path, './images/' . $fname);
+    rename($tmp_file_path, '../images/' . $fname);
     $sql = "UPDATE lucks set attach_filename = '" . $fname . "' WHERE id = ".$id;
     error_log("sql=" . $sql);
     $stm = $pdo->prepare($sql);
