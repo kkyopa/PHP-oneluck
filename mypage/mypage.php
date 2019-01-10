604ae7500ec3f54b1aa11ad2d2ef0bb67ca6d07f
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2019-01-10 19:59:50 +0900

    コメントアウトやインデントを揃える

--- mypage/mypage.php
+++ mypage/mypage.php
@@ -1,32 +1,26 @@
-<input type="button" onClick="location.href='http://192.168.33.10:3000/luckfile/index.php'" value="みんなの投稿"><br><br>
-
+<p><input type="button" onClick="location.href='http://192.168.33.10:3000/luckfile/index.php'" value="みんなの投稿"></p>
 <?php
 require_once('../config.php');
 session_start();
-//var_dump($_SESSION);die;
-if(! isset($_SESSION['id']))
-{
-      print'ログインされていません。<br />';
-      print'<a href="../login/">login</a>';
-      exit();
+if(! isset($_SESSION['id'])) {
+    print'ログインされていません。<br />';
+    print'<a href="../login/">login</a>';
+    exit();
 } else {
-
     require_once('../config.php');
     require_once('model/mypage.php');
     $dbh=new PDO('mysql:host=127.0.0.1;dbname=one_luck;charset=utf8', 'root', DB_PASSWORD);
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $mypage = new Mypage();
     $rec = $mypage->getMypageByUsers($name);
-
     echo $rec["name"]."さん、ようこそ<br>";
     echo "メールアドレス".$rec["email"];
     echo "<br>";
     echo "プロフィール画像";
     echo "<br>";
-    if ($rec['attach_filename']) {
-      echo "<td><img src='../login/mypage_images/" . $rec['attach_filename'] . "'/></td>";
-    } else {
-      echo "<td>no image</td>";
-    }
+        if ($rec['attach_filename']) {
+            echo "<td><img src='../login/mypage_images/" . $rec['attach_filename'] . "'/></td>";
+        } else {
+            echo "<td>no image</td>";
+        }
 }
-?>
