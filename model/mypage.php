79679166646ed6bd97f130a5bcc0b533348df50a
kyosukekawada <kyosukekawada@kyosukekawadanoMacBook-Pro.local>
2019-01-11 16:41:50 +0900

    グローバル変数を使用しない

--- model/mypage.php
+++ model/mypage.php
@@ -1,22 +1,19 @@
 <?php
+$session = $_SESSION['id'];
 class Mypage
 {
     private $db;
     function __construct()
     {
-        $user = root;
-        $password = hoge;
-        $dbname = 'one_luck';
-        $host = 'localhost';
-        $dsn = "mysql:host={$host};dbname={$dbname};charset=utf8";
-        $this->db = new PDO($dsn, $user, $password);
+      require_once('../config.php');
+      $pdo = new PDO('mysql:host=127.0.0.1;dbname=one_luck;charset=utf8', 'root', DB_PASSWORD);
     }
 
-    public function getIndexByName($name)
+    public function getIndexByName($record, $session)
     {
         $sql='SELECT name FROM mypage WHERE id=?';
         $stmt=$this->db->prepare($sql);
-        $params = [$_SESSION['id']];
+        $params = $session;
         $stmt->execute($params);
         $record = $stmt->fetch(PDO::FETCH_ASSOC);
         return $record;
@@ -38,17 +35,15 @@ class Mypage
         $data[]=$mypage_email;
         $data[]=$mypage_pass;
         $stmt->execute($data);
-        $this->db=null;
         $rec=$stmt->fetch(PDO::FETCH_ASSOC);
         return $rec;
     }
 
-    public function getMypageByUsers($name)
+    public function getMypageByUsers($rec, $session)
     {
         $sql='SELECT * FROM mypage WHERE id=?';
         $stmt=$this->db->prepare($sql);
-        $stmt->execute([$_SESSION['id']]);
-        $this->db=null;
+        $stmt->execute($session);
         $rec=$stmt->fetch(PDO::FETCH_ASSOC);
         return $rec;
     }
